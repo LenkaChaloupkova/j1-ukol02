cf576a024cccd9934196249f1781d08142077640
mac <lenkschaloupkova@gmail.com>
2023-03-07 10:04:23 +0100

    (Homework 02) Changes by bmbc

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -15,36 +15,70 @@ public class HlavniProgram {
         nakresliZmrzlinu();
 
         //SNEHULAK
-        zofka.setLocation(330,100);
-        nakresliBarevneKolecko(0.7,new Color(1,0,0));
-        zofka.setLocation(350,203);
-        nakresliBarevneKolecko(1, Color.black);
-        zofka.setLocation(375,332);
-        nakresliBarevneKolecko(1.3, Color.black);
-        zofka.setLocation(390,230);
-        nakresliBarevneKolecko(0.5, Color.black);
-        zofka.setLocation(250,230);
-        nakresliBarevneKolecko(0.5,Color.black);
+//        zofka.setLocation(330,100);
+//        nakresliBarevneKolecko(10,new Color(1,0,0));
+//        zofka.setLocation(310,150);
+//        nakresliBarevneKolecko(20, Color.black);
+//        zofka.setLocation(375,332);
+//        nakresliBarevneKolecko(10, Color.black);
+//        zofka.setLocation(390,230);
+//        nakresliBarevneKolecko(10, Color.black);
+//        zofka.setLocation(250,230);
+//        nakresliBarevneKolecko(10,Color.black);
 
-        //NAKLADAK
-        zofka.setLocation(660,400);
-        nakresliBarevnyObdelnik(100,200,Color.black);
+        //MASINKA
+        nakresliMasinku();
+    }
+
+    private void nakresliZmrzlinu() {
+        zofka.setLocation(200, 100);
+        nakresliBarevneKolecko(10, new Color(138, 43, 226));
+        zofka.setLocation(149, 107);
+        zofka.turnLeft(90.0);
+        nakresliBarevnyRovnostrannyTrojuhelnik(51, new Color(210, 105, 30));
+        zofka.turnRight(90);
+    }
+
+    private void nakresliMasinku() {
+        // telo masinky
+        zofka.turnLeft(90);
+        zofka.setLocation(660, 400);
+        nakresliBarevnyObdelnik(200, 100, Color.black);
+        zofka.turnRight(90);
+        zofka.setLocation(zofka.getX() + 100, zofka.getY() + 100);
+        nakresliBarevnyObdelnik(200, 100, Color.black);
+        zofka.turnRight(90);
+        //kolecka
+        zofka.turnLeft(90);
+        nakresliBarevneKolecko(18, Color.CYAN);
+        zofka.setX(zofka.getX() - 160);
+        zofka.turnLeft(90);
+        nakresliBarevneKolecko(7, Color.black);
+        zofka.setX(zofka.getX() - 80);
+        nakresliBarevneKolecko(7, Color.black);
+        zofka.setX(zofka.getX() - 60);
+        zofka.turnRight(90);
+        zofka.setY(zofka.getY() - 50);
+        nakresliBarevnyRovnostrannyTrojuhelnik(100, Color.BLUE);
     }
 
     public void nakresliBarevnyRovnostrannyTrojuhelnik(double velikostStrany, Color color) {
         zofka.setPenColor(color);
+        zofka.penDown();
 
-        zofka.turnLeft(90.0);
-        zofka.move(velikostStrany);
         for (int i = 0; i < 3; i++) {
             zofka.turnLeft(120);
             zofka.move(velikostStrany);
         }
+
+        zofka.penUp();
     }
+
+
     public void nakresliBarevnyCtverec(double velikost, Color color) {
         zofka.setPenColor(color);
-
         zofka.penDown();
+
         for (int i = 0; i < 4; i++) {
             zofka.move(velikost);
             zofka.turnLeft(90);
@@ -54,16 +88,15 @@ public class HlavniProgram {
 
     private void nakresliBarevnyObdelnik(double delkaStranyA, double delkaStranyB, Color color) {
         zofka.setPenColor(color);
-
         zofka.penDown();
-        zofka.move(delkaStranyA);
 
-        zofka.turnLeft(90);
-        zofka.move(delkaStranyB);
-        zofka.turnLeft(90);
-        zofka.move(delkaStranyA);
-        zofka.turnLeft(90);
-        zofka.move(delkaStranyB);
+        for (int i = 0; i < 2; i++) {
+            zofka.move(delkaStranyA);
+            zofka.turnLeft(90);
+            zofka.move(delkaStranyB);
+            zofka.turnLeft(90);
+        }
+
         zofka.penUp();
     }
 
@@ -71,9 +104,11 @@ public class HlavniProgram {
         zofka.setPenColor(color);
 
         zofka.penDown();
-        for (var o = 0; o < 360; o++) {
+        for (var o = 0; o < 18; o++) {
             zofka.move(delkaStrany);
-            zofka.turnLeft(1); }
+            zofka.turnLeft(20);
+        }
+        zofka.penUp();
     }
 
     public static void main(String[] args) {
